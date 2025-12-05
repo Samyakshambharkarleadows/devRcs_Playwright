53d987b293ffa2a71b0485abf70a14ae77796a3d
Samyakshambharkarleadows <samyak.shambharkar@leadows.com>
2025-12-05 13:00:21 +0530

    /auth/login added in each file goto

--- tests/17-createSuperAdmin.spec.js
+++ tests/17-createSuperAdmin.spec.js
@@ -5,7 +5,8 @@ const { postMessageToThread } = require('worker_threads')
 
 test('Launch Application', async({page}) => {
 
-    await page.goto('https://qarcs.pinlab.in/auth/login')
+    // await page.goto('https://qarcs.pinlab.in/auth/login')
+    await page.goto('/auth/login')
 
     // test.setTimeout(0); // disables test timeout (never stops)
 
